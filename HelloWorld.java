5046b6e5831e7fc960ba380a090a6e8c7044df06
abhishek <abhishek@gmail.com>
2020-02-13 18:08:20 +0530

    done new change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
-    class HelloWorld{  
-        public static void main(String args[]){  
-         System.out.println("Hello Java");  
+    class HelloWorld
+{  
+        public static void main(String args[])
+	{  
+        	System.out.println("Hello Java");
+		int a,b=5;
+		System.out.println("Hello world");
         }  
-    }  
+}  
